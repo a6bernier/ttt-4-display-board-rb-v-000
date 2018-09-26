9f07273df9b7de5695fc5870e24926c4ec9e1dde
Andrew Bernier <a6bernier@gmail.com>
2018-09-26 21:10:24 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 
   board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-def display_board(board[5])
+def display_board(board[4])
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
